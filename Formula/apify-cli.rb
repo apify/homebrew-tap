70a5caabe7c7751d058ece3a9768e3a86e69e04a
František Nesveda <frantisek@apify.com>
2023-08-29 15:08:19 +0200

    Improve the `apify-cli` formula tests

--- Formula/apify-cli.rb
+++ Formula/apify-cli.rb
@@ -12,11 +12,16 @@ class ApifyCli < Formula
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     # We have to replace the shebang in the main executable from "/usr/bin/env node" to point to the keg-only node@16
-    inreplace "#{libexec}/lib/node_modules/apify-cli/src/bin/run", "/usr/bin/env node", "#{Formula["node@16"].opt_bin}/node"
+    inreplace "#{libexec}/lib/node_modules/apify-cli/src/bin/run", "/usr/bin/env node",
+                                                                   "#{Formula["node@16"].opt_bin}/node"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
+    # Test that the Apify CLI is at all installed and working
     assert_match "apify-cli/#{version}", shell_output("#{bin}/apify --version")
+    # Test that the CLI can initialize a new actor
+    system "#{bin}/apify", "init", "testing-actor"
+    assert_predicate testpath/".actor/actor.json", :exist?
   end
 end
