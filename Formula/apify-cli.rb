1e4b712aa4626a9ace727b431ada8841a889ee5e
František Nesveda <frantisek@apify.com>
2023-08-29 16:23:18 +0200

    Update `apify-cli` to use `node@20`

--- Formula/apify-cli.rb
+++ Formula/apify-cli.rb
@@ -7,13 +7,13 @@ class ApifyCli < Formula
   sha256 "28d4e8b032992560a4f842a44e8d107693b1a031560325b90967245c4ed7b628"
   license "Apache-2.0"
 
-  depends_on "node@16"
+  depends_on "node@20"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     # We have to replace the shebang in the main executable from "/usr/bin/env node" to point to the keg-only node@16
     inreplace "#{libexec}/lib/node_modules/apify-cli/src/bin/run", "/usr/bin/env node",
-                                                                   "#{Formula["node@16"].opt_bin}/node"
+                                                                   "#{Formula["node@20"].opt_bin}/node"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
