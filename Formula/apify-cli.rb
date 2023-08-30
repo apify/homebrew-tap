f51cf55cd083f886c5de51192822e5cce18be9d7
František Nesveda <frantisek@apify.com>
2023-08-30 10:16:27 +0200

    Migrate `apify-cli` to `homebrew-core`

--- Formula/apify-cli.rb
+++ Formula/apify-cli.rb
@@ -1,19 +1,28 @@
 require "language/node"
 
 class ApifyCli < Formula
+  include Language::Node::Shebang
+
   desc "Apify command-line interface"
   homepage "https://docs.apify.com/cli"
   url "https://registry.npmjs.org/apify-cli/-/apify-cli-0.18.0.tgz"
   sha256 "28d4e8b032992560a4f842a44e8d107693b1a031560325b90967245c4ed7b628"
   license "Apache-2.0"
 
-  depends_on "node@20"
+  depends_on "node"
+
+  # TODO: move to `disable!` in about a year
+  deprecate! date: "2023-08-30", because: "was moved to homebrew-core, use `brew install apify-cli` instead"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    # We have to replace the shebang in the main executable from "/usr/bin/env node" to point to the keg-only node@16
-    inreplace "#{libexec}/lib/node_modules/apify-cli/src/bin/run", "/usr/bin/env node",
-                                                                   "#{Formula["node@20"].opt_bin}/node"
+    # We have to replace the shebang in the main executable from "/usr/bin/env node"
+    # to point to the Homebrew-provided `node`,
+    # because otherwise the CLI will run with the system-provided Node.js,
+    # which might be a different version than the one installed by Homebrew,
+    # causing issues that `node_modules` were installed with one Node.js version
+    # but the CLI is running them with another Node.js version.
+    rewrite_shebang detected_node_shebang, libexec/"lib/node_modules/apify-cli/src/bin/run"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
