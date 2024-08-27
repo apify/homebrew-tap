f080d34ca043d4ed9ac37b3c54e95595d2fb4553
Apify Service Account <64261774+apify-service-account@users.noreply.github.com>
2024-08-27 15:52:54 +0000

    Updating `apify-cli` to version `0.20.5`

--- Formula/apify-cli.rb
+++ Formula/apify-cli.rb
@@ -5,8 +5,8 @@ class ApifyCli < Formula
 
   desc "Apify command-line interface"
   homepage "https://docs.apify.com/cli"
-  url "https://registry.npmjs.org/apify-cli/-/apify-cli-0.20.3.tgz"
-  sha256 "475252be968ede54a1bc58143b58d47d7c364f6cc21f416b18b2d8ac3dba9ee0"
+  url "https://registry.npmjs.org/apify-cli/-/apify-cli-0.20.5.tgz"
+  sha256 "c0f74e971acea2badd92a2e44e308392e46962656125dd69e50304f6f542cec9"
   license "Apache-2.0"
 
   # TODO: move to `disable!` in about a year
